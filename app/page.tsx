45bae74b1f42c3da244d2ef03ac3a36783584af8
apaul02 <captainankan@gmail.com>
2025-08-24 20:14:38 +0530

    pls work

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import { CreatePosts } from "@/components/createPost";
+import { NewComponent } from "@/components/newComponent";
 
 export default async function Home() {
 
   return (
         <div>
-        <CreatePosts /> 
+            <CreatePosts />
+            <NewComponent />
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+            
         </div>
  );
 }
